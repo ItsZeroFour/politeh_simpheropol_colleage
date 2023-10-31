f31498697e522fd83c94b7a8f24a8742badadaa6
ItsZeroFour <ItsZeroFourX@gmail.com>
2023-10-31 11:27:05 +0300

    server files

---
+++ server/utils/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, proccess.env.SECRET);
+
+      req.userId = decoded._id;
+
+      next();
+    } catch (error) {
+      return res.status(500).send({
+        message: "Access denied",
+      });
+    }
+  } else {
+    return res.status(500).send({
+      message: "Access denied",
+    });
+  }
+};
---
+++ server/utils/checkUserIsAdmin.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export default async (req, res, next) => {
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET);
+
+      const user = await User.findById(decoded);
+
+      if (user.isAdmin) {
+        return next();
+      }
+
+      res.status(500).send({
+        message: "You is not Admin",
+      });
+    } catch (err) {
+      return res.status(500).send({
+        message: "Access denied",
+      });
+    }
+  } else {
+    return res.status(500).send({
+      message: "Access denied",
+    });
+  }
+};
