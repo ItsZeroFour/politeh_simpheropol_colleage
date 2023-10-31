df76655fb6987f5fa4aea1bc7685d8ff7abc234c
AmetDev <ametsejdaliev@gmail.com>
2023-10-31 22:34:37 +0300

    comment-error-in-router

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -12,3 +12,6 @@ router.post('/register', registerValidation, authcontroller.registration)
 router.post('/login', checkAuth, loginValidation, authcontroller.login)
 
 export default router
+//тут проблема короче, я не знаю почему она вызвана
+//я в принципе сделал регистарцию и вход, кроме получения юзеров, но это позже реализую
+//очень странная ошибка из-за которой крашиться приложение сразу же после запуска
