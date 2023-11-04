20295942c7636d2f4822b7430477ca414553e60e
wordite <wordite123@gmail.com>
2023-11-04 17:32:30 +0300

    add alias for public, home/firstscreen in progress

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,7 +1,9 @@
+import FirstScreen from "@/widgets/home/FirstScreen/FirstScreen"
+
 export const Home = () => {
     return (
         <section>
-            
+            <FirstScreen />
         </section>
     )
 }
--- client/src/widgets/home/Head.jsx
+++
@@ -1,9 +0,0 @@
-import style from './Home.module.scss'
-
-const Head = () => {
-    return(
-        <div className={style.head}></div>
-    )
-}
-
-export default Head
