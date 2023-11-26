e473d851ca53f8085e1edb8f2fd93bfa57c81655
AmetDev <ametsejdaliev@gmail.com>
2023-11-26 17:38:26 +0300

    form

--- client/src/widgets/Panel/CreatePage.jsx
+++ client/src/widgets/Panel/CreatePage.jsx
@@ -12,6 +12,7 @@ import Vector from '@public/assets/icons/adminicons/Vector'
 import axios from 'axios'
 import { Interweave, Markup } from 'interweave'
 import { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
 import Popup from 'reactjs-popup'
 const CreatePage = () => {
@@ -25,6 +26,8 @@ const CreatePage = () => {
 	const [image, setImage] = useState(null)
 	const [netImage, setNewImage] = useState([])
 	const [isButtonClicked, setIsButtonClicked] = useState(false)
+	const [URLPage, setURLPage] = useState('')
+	const [typePage, setTypePage] = useState('')
 	const searchEvent = e => {}
 	let tmp = '../../app/testing'
 
@@ -480,7 +483,7 @@ const CreatePage = () => {
 			console.log('lastIndex', finis_index)
 			if (initium_index !== -1) {
 				let newSubString =
-					'<div style=" margin-left: 50%">' + selectedText + '</div>'
+					'<div style=" margin-left: 40%">' + selectedText + '</div>'
 				let newString =
 					textValue.substring(0, initium_index) +
 					newSubString +
@@ -625,6 +628,13 @@ const CreatePage = () => {
 	const addPublishLink = e => {
 		setPublishLink(e)
 	}
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+	const onSubmit = data => console.log(data)
+	console.log(errors)
 
 	const addPageInServer = async () => {
 		let newUrl = ''
@@ -640,187 +650,246 @@ const CreatePage = () => {
 			console.log(someDate.data.pageUrl)
 		}
 	}
+	const [isPage, setIsPage] = useState(true)
 
-	return (
-		<div className='flex flex-col'>
+	const AdminPanel = ({ props }) => {
+		return (
 			<div>
-				<Counter />
+				{!props && (
+					<div className='flex flex-col'>
+						<div>
+							<Counter />
+							<Popup
+								trigger={<button> Normal text</button>}
+								position='bottom center'
+							>
+								<div>
+									<button onClick={() => handleDeleteTegs()}>
+										Normal text
+									</button>
+									<button>
+										<h1 onClick={() => handleAddHeadingOne()}>Heading 1</h1>
+									</button>
+									<button onClick={() => handleAddHeadingTwo()}>
+										<h2>Heading 2</h2>
+									</button>
+									<button onClick={() => handleAddHeadingThree()}>
+										<h3>Heading 3</h3>
+									</button>
+								</div>
+							</Popup>
+							<Popup
+								trigger={
+									<button
+										onClick={() => setIsButtonClicked(true)}
+										style={{ marginLeft: 10 }}
+									>
+										<div style={{ display: 'flex', alignItems: 'center' }}>
+											<AlignLeft /> <Vector />
+										</div>
+									</button>
+								}
+								position='bottom center'
+							>
+								<div
+									style={{
+										backgroundColor: 'gray',
+										display: 'flex',
+										flexDirection: 'column',
+									}}
+								>
+									<button onClick={() => addRight()}>
+										<div style={{ display: 'flex', flexDirection: 'row' }}>
+											<AlignRight />
+											<span style={{ marginLeft: '10px' }}>в право</span>
+										</div>
+									</button>
+									<button onClick={() => addCenter()}>
+										<div style={{ display: 'flex', flexDirection: 'row' }}>
+											<AlignCenter />
+											<span style={{ marginLeft: '10px' }}>центр</span>
+										</div>
+									</button>
+									<button onClick={() => addLeft()}>
+										<div style={{ display: 'flex', flexDirection: 'row' }}>
+											<AlignLeft />
+											<span style={{ marginLeft: '10px' }}>в лево</span>
+										</div>
+									</button>
+								</div>
+							</Popup>
+							<Popup
+								trigger={
+									<button style={{ marginLeft: 10 }}> в лево(текст)</button>
+								}
+								position='bottom center'
+							>
+								<div>
+									<button onClick={() => addRightText()}>right</button>
+									<button onClick={() => addCenterText()}>center</button>
+									<button onClick={() => addLeftText()}>left</button>
+								</div>
+							</Popup>
+							<button style={{ marginLeft: 10 }} onClick={() => addParagraph()}>
+								P
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => addStrong()}>
+								<b>B</b>
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => MarkItalics()}>
+								<i>I</i>
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => addUnderline()}>
+								<u>U</u>
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => addStrike()}>
+								<s>S</s>
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => addList()}>
+								spisok
+							</button>
+							<button style={{ marginLeft: 10 }} onClick={() => addListNum()}>
+								spisokNum
+							</button>
+							<Popup
+								trigger={<button style={{ marginLeft: 10 }}>LINK</button>}
+								position='right center'
+							>
+								<div>
+									<label htmlFor=''>
+										<input
+											type='text'
+											onChange={e => addNameLink(e)}
+											placeholder='заголовок ссылки'
+										/>
+										<input
+											onChange={e => addLink(e)}
+											type='text'
+											placeholder='ссылка'
+										/>
+										<button onClick={() => addLinkElement()}>+</button>
+									</label>
+								</div>
+							</Popup>
+							<Popup
+								trigger={
+									<button
+										style={{
+											marginLeft: 10,
+											padding: 0,
+											border: 'none',
+											font: 'inherit',
+											color: 'inherit',
+											backgroundColor: 'transparent',
+										}}
+									>
+										image
+									</button>
+								}
+								position='right center'
+							>
+								<div>
+									<form onSubmit={submitForm}>
+										<input type='text' id='name' ref={nameRef} />
+										<input
+											id='files'
+											ref={filesRef}
+											multiple
+											type='file'
+											className='image-button'
+											onChange={handleImageChange}
+										/>
+										<button type='submit'>Submit</button>
+									</form>
+								</div>
+							</Popup>
 
-				<Popup trigger={<button> Normal text</button>} position='bottom center'>
-					<div>
-						<button onClick={() => handleDeleteTegs()}>Normal text</button>
-						<button>
-							<h1 onClick={() => handleAddHeadingOne()}>Heading 1</h1>
-						</button>
-						<button onClick={() => handleAddHeadingTwo()}>
-							<h2>Heading 2</h2>
-						</button>
-						<button onClick={() => handleAddHeadingThree()}>
-							<h3>Heading 3</h3>
-						</button>
-					</div>
-				</Popup>
-				<Popup
-					trigger={
-						<button
-							onClick={() => setIsButtonClicked(true)}
-							style={{ marginLeft: 10 }}
-						>
-							<div style={{ display: 'flex', alignItems: 'center' }}>
-								<AlignLeft /> <Vector />
-							</div>
-						</button>
-					}
-					position='bottom center'
-				>
-					<div
-						style={{
-							backgroundColor: 'gray',
-							display: 'flex',
-							flexDirection: 'column',
-						}}
-					>
-						<button onClick={() => addRight()}>
-							<div style={{ display: 'flex', flexDirection: 'row' }}>
-								<AlignRight />
-								<span style={{ marginLeft: '10px' }}>в право</span>
-							</div>
-						</button>
-						<button onClick={() => addCenter()}>
-							<div style={{ display: 'flex', flexDirection: 'row' }}>
-								<AlignCenter />
-								<span style={{ marginLeft: '10px' }}>центр</span>
-							</div>
-						</button>
-						<button onClick={() => addLeft()}>
-							<div style={{ display: 'flex', flexDirection: 'row' }}>
-								<AlignLeft />
-								<span style={{ marginLeft: '10px' }}>в лево</span>
-							</div>
-						</button>
-					</div>
-				</Popup>
-				<Popup
-					trigger={<button style={{ marginLeft: 10 }}> в лево(текст)</button>}
-					position='bottom center'
-				>
-					<div>
-						<button onClick={() => addRightText()}>right</button>
-						<button onClick={() => addCenterText()}>center</button>
-						<button onClick={() => addLeftText()}>left</button>
-					</div>
-				</Popup>
-				<button style={{ marginLeft: 10 }} onClick={() => addParagraph()}>
-					P
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => addStrong()}>
-					<b>B</b>
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => MarkItalics()}>
-					<i>I</i>
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => addUnderline()}>
-					<u>U</u>
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => addStrike()}>
-					<s>S</s>
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => addList()}>
-					spisok
-				</button>
-				<button style={{ marginLeft: 10 }} onClick={() => addListNum()}>
-					spisokNum
-				</button>
-				<Popup
-					trigger={<button style={{ marginLeft: 10 }}>LINK</button>}
-					position='right center'
-				>
-					<div>
-						<label htmlFor=''>
-							<input
-								type='text'
-								onChange={e => addNameLink(e)}
-								placeholder='заголовок ссылки'
-							/>
-							<input
-								onChange={e => addLink(e)}
-								type='text'
-								placeholder='ссылка'
-							/>
-							<button onClick={() => addLinkElement()}>+</button>
-						</label>
-					</div>
-				</Popup>
-				<Popup
-					trigger={
-						<button
+							<Popup
+								trigger={<button style={{ marginLeft: 30 }}>ADD PAGE</button>}
+								position='right center'
+							>
+								<div>
+									<label htmlFor=''>
+										<input
+											style={{
+												backgroundColor: '#000',
+											}}
+											onChange={e => addPublishLink(e.target.value)}
+											type='text'
+											placeholder='адрес страницы'
+										/>
+										<button onClick={() => addPageInServer()}>
+											ОПУБЛИКОВАТЬ
+										</button>
+									</label>
+								</div>
+							</Popup>
+						</div>
+						<textarea
+							onChange={e => dispatch(textValueFunc(e.target.value))}
+							onMouseUp={e => handleFocus(e)}
+							value={textValue}
+							onKeyDown={handleKeyPress}
 							style={{
-								marginLeft: 10,
-								padding: 0,
-								border: 'none',
-								font: 'inherit',
-								color: 'inherit',
-								backgroundColor: 'transparent',
+								width: '100%',
+								height: '200px',
+								backgroundColor: 'black',
+							}}
+							type='text'
+							placeholder='Начните писать...'
+						/>
+						<RenderAll />
+					</div>
+				)}
+			</div>
+		)
+	}
+	const CheckerPopup = ({ props }) => {
+		return (
+			<div>
+				{props && (
+					<div>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<input
+								style={{ backgroundColor: 'black' }}
+								type='text'
+								placeholder='URLPage:'
+								{...register('URLPage:')}
+							/>
+							<label>Наш колледж</label>
+							<input {...register('TypePage')} type='radio' value='own' />
+							<label>Посты</label>
+							<input {...register('TypePage')} type='radio' value='post' />
+							<input type='submit' />
+						</form>
+						<button
+							onClick={async () => {
+								setIsPage(false)
+								try {
+									await axios.post(
+										'http://localhost:5000/page/create',
+										{ typePage, URLPage },
+										{
+											headers: { 'Access-Control-Allow-Origin': '*' },
+										}
+									)
+								} catch (error) {
+									console.log(error)
+								}
+								console.log('true', isPage)
 							}}
 						>
-							image
+							<span>Загрузить страницу</span>
 						</button>
-					}
-					position='right center'
-				>
-					<div>
-						<form onSubmit={submitForm}>
-							<input type='text' id='name' ref={nameRef} />
-							<input
-								id='files'
-								ref={filesRef}
-								multiple
-								type='file'
-								className='image-button'
-								onChange={handleImageChange}
-							/>
-							<button type='submit'>Submit</button>
-						</form>
 					</div>
-				</Popup>
-
-				<Popup
-					trigger={<button style={{ marginLeft: 30 }}>ADD PAGE</button>}
-					position='right center'
-				>
-					<div>
-						<label htmlFor=''>
-							<input
-								style={{
-									backgroundColor: '#000',
-								}}
-								onChange={e => addPublishLink(e.target.value)}
-								type='text'
-								placeholder='адрес страницы'
-							/>
-							<button onClick={() => addPageInServer()}>ОПУБЛИКОВАТЬ</button>
-						</label>
-					</div>
-				</Popup>
+				)}
 			</div>
-			<textarea
-				onChange={e => dispatch(textValueFunc(e.target.value))}
-				onMouseUp={e => handleFocus(e)}
-				value={textValue}
-				onKeyDown={handleKeyPress}
-				style={{ width: '100%', height: '200px', backgroundColor: 'black' }}
-				type='text'
-				placeholder='Начните писать...'
-			/>
-			{/* {images.map(el => {
-				return <img src={el} alt='Uploaded' style={{ maxWidth: '600px' }} />
-			})} */}
-			{/* <img src={netImage} width={100} height={100} /> */}
-			{/* {netImage.map(el => {
-				return <img src={el.myFile} width={500} alt='' />
-			})} */}
-			<MarkedElement italic={italic} />
-			<RenderAll />
+		)
+	}
+	return (
+		<div>
+			<CheckerPopup props={isPage} />
+			<AdminPanel props={isPage} />
 		</div>
 	)
 }
