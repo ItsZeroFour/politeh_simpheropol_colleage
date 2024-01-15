f1a648532d59d589311198d84d0bb641f60e2e8d
AmetDev <ametsejdaliev@gmail.com>
2024-01-15 22:31:25 +0300

    schedule-vers

--- client/src/pages/Schedule/Schedule.jsx
+++ client/src/pages/Schedule/Schedule.jsx
@@ -1,70 +1,91 @@
 'use client'
 
-import Corpus from '@widgets/Schedule/Corpus/Corpus'
-import style from './Schedule.module.scss'
-import { useDispatch, useSelector } from 'react-redux'
-import { getSchedule } from '@app/store/schedule/schedule.slice'
-import Favourited from '@features/Schedule/Favourited/Favourited'
-import { stateSave } from '@processes/stateSaver/StateSaver'
-import { getSavedState } from '@processes/getSavedState/getSavedState'
 import { useActions } from '@app/hooks/useActions'
-import { useEffect } from 'react'
-import { useLocalStorage } from '@uidotdev/usehooks'
 import DaySelector from '@entities/Schedule/DaySelector/DaySelector'
-
-
+import Favourited from '@features/Schedule/Favourited/Favourited'
+import Corpus from '@widgets/Schedule/Corpus/Corpus'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
 
-
 const scheduleOfGroup = [
-  { name: 'основы жизни', lecturer: 'Власенко', cabinet: 225 },
-  { name: 'инфор. технологии', lecturer: 'Акимова', cabinet: 307 },
-  { name: 'БЖД', lecturer: 'Маланьин', cabinet: 216 },
-  {},
-  { name: 'технические штуки', lecturer: 'Клементьев', cabinet: 109 },
-  { name: 'история', lecturer: 'Пшеничная', cabinet: 112 },
+	{ name: 'основы жизни', lecturer: 'Власенко', cabinet: 225 },
+	{ name: 'инфор. технологии', lecturer: 'Акимова', cabinet: 307 },
+	{ name: 'БЖД', lecturer: 'Маланьин', cabinet: 216 },
+	{},
+	{ name: 'технические штуки', lecturer: 'Клементьев', cabinet: 109 },
+	{ name: 'история', lecturer: 'Пшеничная', cabinet: 112 },
 ]
 
 const scheduleLine = [
-  ['12ИСП-В', scheduleOfGroup],
-  ['22ГГВП', scheduleOfGroup],
-  ['12ИСП-П', scheduleOfGroup],
-  ['23ИСП-В', scheduleOfGroup],
-  ['12ИМД', scheduleOfGroup],
-  ['12ИСП-В', scheduleOfGroup],
+	['12ИСП-В', scheduleOfGroup],
+	['22ГГВП', scheduleOfGroup],
+	['12ИСП-П', scheduleOfGroup],
+	['23ИСП-В', scheduleOfGroup],
+	['12ИМД', scheduleOfGroup],
+	['12ИСП-В', scheduleOfGroup],
 ]
 
 const scheduleData = {
-  monday: [ scheduleLine, scheduleLine, scheduleLine, scheduleLine, scheduleLine ],
-  tuesday: [ scheduleLine, scheduleLine, scheduleLine, scheduleLine, scheduleLine ],
-  wednesday: [ scheduleLine, scheduleLine, scheduleLine, scheduleLine, scheduleLine ],
-  thursday: [ scheduleLine, scheduleLine, scheduleLine, scheduleLine, scheduleLine ],
-  friday: [ scheduleLine, scheduleLine, scheduleLine, scheduleLine, scheduleLine ],
+	monday: [
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+	],
+	tuesday: [
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+	],
+	wednesday: [
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+	],
+	thursday: [
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+	],
+	friday: [
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+		scheduleLine,
+	],
 }
 
 const data = {
-  first: scheduleData,
-  second: scheduleData,
+	first: scheduleData,
+	second: scheduleData,
 }
 
 const Schedule = () => {
-  const dispatch = useDispatch()
-  const { loadState } = useActions()
+	const dispatch = useDispatch()
+	const { loadState } = useActions()
 
-  useEffect(() => {
-    dispatch(loadState())
-  }, [])
+	useEffect(() => {
+		dispatch(loadState())
+	}, [])
 
-
-  return (
-    <section>
-      <DaySelector  />
-      <Favourited data={data} />
-      <Corpus title='Первый корпус' data={data.first} />
-      <Corpus title='Второй корпус' data={data.second} />
-    </section>
-  )
+	return (
+		<section>
+			<DaySelector />
+			<Favourited data={data} />
+			<Corpus title='Первый корпус' data={data.first} />
+			<Corpus title='Второй корпус' data={data.second} />
+		</section>
+	)
 }
 
 export default Schedule
--- client/src/widgets/home/ContactForm/ContactForm.jsx
+++ client/src/widgets/home/ContactForm/ContactForm.jsx
@@ -1,39 +1,57 @@
 'use client'
 
+import { useInput } from '@app/hooks/useInput'
+import VK from '@public/assets/icons/vk.svg'
 import Link from 'next/link'
 import style from './ContactForm.module.scss'
-import VK from '@public/assets/icons/VK.svg'
-import { useInput } from '@app/hooks/useInput'
 
 const ContactForm = () => {
-  const { value: fullname, bind: fullnameBind } = useInput()
-  const { value: email, bind: emailBind } = useInput()
-  const { value: theme, bind: themeBind } = useInput()
-  const { value: message, bind: messageBind } = useInput()
+	const { value: fullname, bind: fullnameBind } = useInput()
+	const { value: email, bind: emailBind } = useInput()
+	const { value: theme, bind: themeBind } = useInput()
+	const { value: message, bind: messageBind } = useInput()
 
+	return (
+		<div className={style.contact}>
+			<div className={style.top}>
+				<p className={style.title}>Свяжитесь с нами!</p>
+				<div className={style.icon}>
+					<Link href='/'>
+						<VK />
+					</Link>
+				</div>
+			</div>
 
-  return <div className={style.contact}>
+			<form className={style.form}>
+				<input
+					{...fullnameBind}
+					type='text'
+					className={style.input}
+					placeholder='Ваше ФИО'
+				/>
+				<input
+					{...emailBind}
+					type='text'
+					className={style.input}
+					placeholder='Ваш E-Mail'
+				/>
+				<input
+					{...themeBind}
+					type='text'
+					className={style.input}
+					placeholder='Тема'
+				/>
 
-    <div className={style.top}>
-      <p className={style.title}>Свяжитесь с нами!</p>
-      <div className={style.icon}>
-        <Link href='/'>
-          <VK />
-        </Link>
-      </div>
-    </div>
+				<textarea
+					{...messageBind}
+					className={style.input + ' ' + style.textarea}
+					placeholder='Ваше сообщение...'
+				/>
 
-    <form className={style.form}>
-      <input {...fullnameBind} type="text" className={style.input} placeholder='Ваше ФИО' />
-      <input {...emailBind} type="text" className={style.input} placeholder='Ваш E-Mail' />
-      <input {...themeBind} type="text" className={style.input} placeholder='Тема' />
-
-      <textarea {...messageBind} className={style.input + ' ' + style.textarea} placeholder='Ваше сообщение...'/>
-
-      <button className={style.submit}>Отправить</button>
-    </form>
-  </div>
+				<button className={style.submit}>Отправить</button>
+			</form>
+		</div>
+	)
 }
 
 export default ContactForm
-
