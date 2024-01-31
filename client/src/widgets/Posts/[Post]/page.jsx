80c60094e5528e22fa1972d15c1936d18e4bb0e8
wordite <wordite123@gmail.com>
2024-01-31 10:56:07 +0300

    fixed post go back button

--- client/src/widgets/Posts/[Post]/page.jsx
+++ client/src/widgets/Posts/[Post]/page.jsx
@@ -1,14 +1,20 @@
-import axios from "axios";
-import { Interweave } from "interweave";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import style from "./page.module.scss";
-import EyeImg from "@public/assets/icons/eye.svg";
+import axios from 'axios'
+import { Interweave } from 'interweave'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import style from './page.module.scss'
+import EyeImg from '@public/assets/icons/eye.svg'
+import { useRouter } from 'next/navigation'
 
 export default function Post({ params }) {
-  const [data, setData] = useState({});
-  const [allData, setAllData] = useState([]);
-  const someData = useSelector((state) => state.counter);
+  const [data, setData] = useState({})
+  const [allData, setAllData] = useState([])
+  const someData = useSelector((state) => state.counter)
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.back()
+  }
 
   useEffect(() => {
     const someAsyncFunc = async () => {
@@ -16,16 +22,16 @@ export default function Post({ params }) {
         const somedata = await axios.get(
           `${process.env.NEXT_PUBLIC_SERVER_URL}/page/getpagecontent`,
           { params: { postId: params.postId } }
-        );
+        )
 
-        setData({ ...somedata.data });
-        document.body.scrollTop = 0;
+        setData({ ...somedata.data })
+        document.body.scrollTop = 0
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
-    };
-    someAsyncFunc();
-  }, []);
+    }
+    someAsyncFunc()
+  }, [])
 
   useEffect(() => {
     const someAsyncFunc = async () => {
@@ -33,23 +39,23 @@ export default function Post({ params }) {
         const mainData = await axios.get(
           `${process.env.NEXT_PUBLIC_SERVER_URL}/page/get`,
           { params: { url: params.postId } }
-        );
+        )
 
-        setAllData(mainData.data);
+        setAllData(mainData.data)
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
-    };
-    someAsyncFunc();
-  }, []);
+    }
+    someAsyncFunc()
+  }, [])
 
   return (
     <main
       className={style.post__main}
       style={{
-        display: "flex",
-        justifyItems: "center",
-        flexDirection: "column",
+        display: 'flex',
+        justifyItems: 'center',
+        flexDirection: 'column',
       }}
     >
       <Interweave content={data.pageContent} />
@@ -64,22 +70,20 @@ export default function Post({ params }) {
       <div style={{ marginTop: 50 }}>
         <button
           style={{
-            width: "100%",
-            margin: "0 auto",
-            display: "block",
-            backgroundColor: "#131313",
+            width: '100%',
+            margin: '0 auto',
+            display: 'block',
+            backgroundColor: '#131313',
             borderRadius: 10,
             padding: 20,
-            color: "#FFF",
+            color: '#FFF',
             fontSize: 24,
           }}
-          onClick={() => {
-            history.back();
-          }}
+          onClick={handleClick}
         >
           Вернуться назад
         </button>
       </div>
     </main>
-  );
+  )
 }
