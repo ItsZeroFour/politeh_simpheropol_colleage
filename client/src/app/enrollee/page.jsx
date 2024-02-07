53b7a7439c9b5da8a0ca286fddb336377f491a96
itszerofour <itszerofourx@gmail.com>
2024-02-07 13:45:49 +0300

    fix

--- client/src/app/enrollee/page.jsx
+++ client/src/app/enrollee/page.jsx
@@ -1,29 +1,30 @@
 import Enrollee from "../../pages/enrollee/Enrollee";
 
-
 const getFiles = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/files/get?forPage=enrollee`, {
-    next: { revalidate: 900 }
-  })
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/files/get?forPage=enrollee`,
+    {
+      next: { revalidate: 1488 },
+    }
+  );
 
-  return await response.json()
-}
-
+  return await response.json();
+};
 
 const getSpecialities = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/speciality/getSpecialities`, {
-    next: { revalidate: 900 }
-  })
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/speciality/getSpecialities`,
+    {
+      next: { revalidate: 1488 },
+    }
+  );
 
-  return await response.json()
-}
-
-
-
+  return await response.json();
+};
 
 const index = async () => {
-  const files = await getFiles()
-  const specialityes = await getSpecialities()
+  const files = await getFiles();
+  const specialityes = await getSpecialities();
 
   return <Enrollee files={files} specialityes={specialityes} />;
 };
