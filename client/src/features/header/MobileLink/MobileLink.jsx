560fe6d8321a0fe755ccb4f2b8b83a27375a02cd
itszerofour <itszerofourx@gmail.com>
2024-06-28 13:53:48 +0300

    fix bugs

--- client/src/features/header/MobileLink/MobileLink.jsx
+++ client/src/features/header/MobileLink/MobileLink.jsx
@@ -4,35 +4,35 @@ import React, { useState } from "react";
 import style from "./../../../widgets/header/header.module.scss";
 import Link from "next/link";
 import Triangle from "@public/assets/icons/triangle.svg";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useActions } from "@app/hooks/useActions";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import axios from "axios";
 
 const MobileLink = React.memo(function MobileLink({ stlye, link }) {
   const dispatch = useDispatch();
   const { setIsOpenedMenu } = useActions();
 
-  const router = useRouter()
-	const Clicked = async url => {
-		try {
-			const somedata = await axios.get(
-				`${process.env.NEXT_PUBLIC_SERVER_URL}/page/getonepage`,
-				{ params: { url } }
-			)
+  const router = useRouter();
+  const Clicked = async (url) => {
+    try {
+      const somedata = await axios.get(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/page/getonepage`,
+        { params: { url } }
+      );
 
-			if (somedata.status == 200) {
-				console.log(somedata.data.message.pageUrl)
-				router.push('/our-colleage/' + somedata.data.message.pageUrl)
-			}
-		} catch (error) {
-			router.push(`${url}`)
-		}
-	}
+      if (somedata.status == 200) {
+        console.log(somedata.data.message.pageUrl);
+        router.push("/our-colleage/" + somedata.data.message.pageUrl);
+      }
+    } catch (error) {
+      router.push(`${url}`);
+    }
+  };
 
   const onMenuClick = (link) => {
     dispatch(setIsOpenedMenu(false));
-    Clicked(link.url)
+    Clicked(link.url);
   };
 
   const [isClicked, setIsClicked] = useState(false);
@@ -57,13 +57,11 @@ const MobileLink = React.memo(function MobileLink({ stlye, link }) {
       id={link.nestedObjects.length !== 0 ? link.text : null}
     >
       {link.nestedObjects.length === 0 ? (
-        <Link href={link.url} onClick={onMenuClick}>
+        <Link href={link.url} onClick={() => dispatch(setIsOpenedMenu(false))}>
           {link.text}
         </Link>
       ) : (
-        <span className="header__link__34">
-          {link.text}
-        </span>
+        <span className="header__link__34">{link.text}</span>
       )}
       {link.nestedObjects.length !== 0 && (
         <Triangle
@@ -83,9 +81,7 @@ const MobileLink = React.memo(function MobileLink({ stlye, link }) {
             <div className={style.mobileDropdownDelimiter}></div>
             {link.nestedObjects.map((link, index) => (
               <li key={index} className={style.link}>
-                <span onClick={() => onMenuClick(link)}>
-                  {link.text}
-                </span>
+                <span onClick={() => onMenuClick(link)}>{link.text}</span>
               </li>
             ))}
           </ul>
