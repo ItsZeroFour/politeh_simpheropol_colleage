0a0b763a1c29b0aa269c45d18d70ddcc1942a25a
ItsZeroFour <itsZeroFourX@gmail.com>
2024-07-05 10:25:26 +0300

    fix

--- client/src/widgets/Enrollee/EnrolleeOtherLinks.jsx
+++ client/src/widgets/Enrollee/EnrolleeOtherLinks.jsx
@@ -6,8 +6,9 @@ import "react-loading-skeleton/dist/skeleton.css";
 const EnrolleeOtherLinks = ({ files }) => {
   return (
     <ol className={style.enrollee__links}>
-      {files
-        ? files.map(({ file, name }) => (
+      {files ? (
+        <>
+          {files.map(({ file, name }) => (
             <li key={name}>
               <Link
                 href={`${process.env.NEXT_PUBLIC_SERVER_URL}/uploads/${file}`}
@@ -17,14 +18,27 @@ const EnrolleeOtherLinks = ({ files }) => {
                 {name}
               </Link>
             </li>
-          ))
-        : [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item_, index) => (
-            <article className={style.enrollee__links__loading} key={index}>
-              <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                <Skeleton className={style.enrollee__links__loading} />
-              </SkeletonTheme>
-            </article>
           ))}
+          <li>
+            <Link
+              href="/usloviya-obucheniya-invalidov"
+              target="_blank"
+              rel="norefferer"
+            >
+              Условия обучения инвалидов и лиц с ограниченными возможностями
+              здоровья
+            </Link>
+          </li>
+        </>
+      ) : (
+        [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item_, index) => (
+          <article className={style.enrollee__links__loading} key={index}>
+            <SkeletonTheme baseColor="#202020" highlightColor="#444">
+              <Skeleton className={style.enrollee__links__loading} />
+            </SkeletonTheme>
+          </article>
+        ))
+      )}
     </ol>
   );
 };
