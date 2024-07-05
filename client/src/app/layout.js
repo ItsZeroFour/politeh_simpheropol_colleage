e7bd9e72006bd7756552d6ddec5cd8cf109bd943
ItsZeroFour <itsZeroFourX@gmail.com>
2024-07-05 11:11:14 +0300

    add new feature

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -69,8 +69,8 @@ const RootLayout = async ({ children }) => {
           </div>
         </StoreProvider>
 
-        {/* <Script src="https://lidrekon.ru/slep/js/jquery.js" />
-        <Script src="https://lidrekon.ru/slep/js/uhpv-full.min.js" /> */}
+        <Script src="https://lidrekon.ru/slep/js/jquery.js" />
+        <Script src="https://lidrekon.ru/slep/js/uhpv-full.min.js" />
       </body>
     </html>
   );
